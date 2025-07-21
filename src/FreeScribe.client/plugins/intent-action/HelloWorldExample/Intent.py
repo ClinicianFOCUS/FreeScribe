6380c2876555fc4cb8b86460e2530bd84a8e0331
Alex Simko <alexsimko4@gmail.com>
2025-07-21 13:32:06 -0400

    feat(HelloWorldExample): Add a example of how to add a registered entity into a plugin.

--- src/FreeScribe.client/plugins/intent-action/HelloWorldExample/Intent.py
+++ src/FreeScribe.client/plugins/intent-action/HelloWorldExample/Intent.py
@@ -2,15 +2,40 @@
 This is an example of a simple intent action plugin that recognizes the phrase "hello world".
 """
 
-from services.intent_actions.intents.spacy_recognizer import SpacyIntentPattern
+from services.intent_actions.intents.spacy_recognizer import SpacyIntentPattern, SpacyEntityPattern
 
 exported_patterns = [
     SpacyIntentPattern(
         intent_name="hello_world",
         patterns=[
             [{"LOWER": "hello"}, {"LOWER": "world"}],
+            [{"LOWER": "hello"}, {"ENT_TYPE": "GREETING_TARGET"}],  # Pattern using custom entity
+            [{"LOWER": "greet"}, {"ENT_TYPE": "GREETING_TARGET"}],
         ],
         required_entities=[],
         confidence_weights={"pattern_match": 1.0, "entity_match": 0.0}
+    ),
+    SpacyIntentPattern(
+        intent_name="greet",
+        patterns=[
+            [{"LOWER": "greet"}, {"ENT_TYPE": "GREETING_TARGET"}],
+            [ {"LOWER": "hello"}, {"ENT_TYPE": "GREETING_TARGET"}],
+        ],
+        required_entities=[],
+        confidence_weights={"pattern_match": 1.0, "entity_match": 0.0}
+    )
+]
+
+exported_entities = [
+    SpacyEntityPattern(
+        entity_name="GREETING_TARGET",
+        patterns=[
+            [{"LOWER": "world"}],
+            [{"LOWER": "universe"}],
+            [{"LOWER": "everyone"}],
+            [{"LOWER": "there"}],
+            [{"LOWER": "friend"}],
+            [{"LOWER": "buddy"}],
+        ]
     )
 ]
